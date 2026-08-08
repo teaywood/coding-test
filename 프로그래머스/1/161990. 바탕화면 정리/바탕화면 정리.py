35bee3f5569f92afe18ebc51155e8c1455dec19d
teaywood <teaywood@gmail.com>
2026-08-08 23:38:57 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.17 ms, Memory: 11.6 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,21 +1,10 @@
 def solution(wallpaper):
-    luy = len(wallpaper[0]) #초기값: max y idx
-    lux = rdx = rdy = -1
-
-    for x, arr in enumerate(wallpaper):
-        for y, doc in enumerate(arr):
-            if doc == '#':
-                #가장 위 좌표 찾기
-                if lux == -1:
-                    lux = x
-                #가장 왼쪽 좌표 찾기
-                if y < luy:
-                    luy = y
-                #가장 아래 좌표 찾기
-                if x+1 > rdx:
-                    rdx = x+1
-                #가장 오른쪽 좌표 찾기
-                if y+1 > rdy:
-                    rdy = y+1
-            
+    coords = [(x, y) for x, row in enumerate(wallpaper) for y, cell in enumerate(row) if cell == '#']
+    
+    rows = [x for x, y in coords]
+    cols = [y for x, y in coords]
+    
+    lux, luy = min(rows), min(cols)
+    rdx, rdy = max(rows)+1, max(cols)+1
+    
     return [lux, luy, rdx, rdy]
